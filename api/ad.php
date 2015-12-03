a9c7dc95c0ded796f83c21e9abc2469716185592
Evgeny <mors741@yandex.ru>
2015-12-03 18:33:23 +0300

    Добавлено три метода в  api объялений: mine, list, add_fav

---
+++ api/ad.php
@@ -0,0 +1,124 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include("general.php");
+
+$mine = function($req) {
+	$error = false;
+	$db = get_db_connection();
+
+	$where = [];
+	if ( array_key_exists('owner', $req) ) {
+		$where['owner'] = $req['owner'];
+	}
+
+	$select = [
+		'id' => 'id',
+		'description' => 'description',
+		'checked' => 'checked',
+		'ts' => 'ts',
+		'owner' => 'owner',
+		'address' => 'address',
+		'categories' => 'categories'
+	];
+
+	$query = create_select($db, 'ad_v', $select, $where);
+	//return $query;
+	$res = $db->query($query) or die ("sql error");
+
+	$rows = [];
+	while ( $order = $res->fetch_array(MYSQLI_ASSOC) ) {
+		$rows[] = $order;
+	}
+	$answer['rows'] = $rows;
+	
+	$res = $db->query("SELECT FOUND_ROWS()");
+	$res = $res->fetch_array(MYSQLI_ASSOC);
+	$answer['total'] = $res["FOUND_ROWS()"];
+
+	$answer['success'] = !$error;
+	return $answer;
+};
+
+$list = function($req) {
+	$error = false;
+	$db = get_db_connection();
+	
+	if ( array_key_exists('key', $req) ) {
+		$key = $req['key'];
+	} else {
+		$key = "";
+	}
+	
+	if ( array_key_exists('cat', $req) ) {
+		$cat = $req['cat'];
+	} else {
+		$cat = "";
+	}
+	
+	$query = "	SELECT * FROM `ad_v`
+				WHERE
+					description LIKE '%".$key."%' AND
+					categories LIKE '%".$cat."%'";
+	$res = $db->query($query) or die ("sql error");
+
+	$rows = [];
+	while ( $order = $res->fetch_array(MYSQLI_ASSOC) ) {
+		$rows[] = $order;
+	}
+	$answer['rows'] = $rows;
+	
+	$res = $db->query("SELECT FOUND_ROWS()");
+	$res = $res->fetch_array(MYSQLI_ASSOC);
+	$answer['total'] = $res["FOUND_ROWS()"];
+
+	$answer['success'] = !$error;
+	return $answer;
+};
+
+$add_fav = function($req) {
+	$error = false;
+	$db = get_db_connection();
+	
+	if (! array_key_exists('user', $req) ) {
+		$answer['success'] = false;
+		$answer['error'] = "user is undefined";
+		return $answer;
+	} 
+	
+	if (! array_key_exists('id', $req) ) {
+		$answer['success'] = false;
+		$answer['error'] = "id is undefined";
+		return $answer;
+	} 
+	$query = "INSERT INTO  `campus`.`favorite` (`aid` , `uid`)
+	VALUES ('".$req['id']."',  '".$req['user']."');";
+	
+	$res = $db->query($query) or die ("sql error");
+
+	$answer['success'] = $res;
+	return $answer;
+};
+
+
+$handlers = [
+	'mine' => $mine,
+	'list' => $list,
+	'add_fav' => $add_fav
+];
+
+$req = get_json();
+$type = $req['type'];
+unset($req['type']);
+if ( !array_key_exists($type, $handlers) ) {
+	die("wrong type");
+}
+
+print (
+	json_encode(
+		$handlers[$type]($req)
+		)
+	);
